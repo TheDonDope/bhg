0824e34012228b13b63d8cbd1275c604a89ea6f3
TheDonDope <thedondope@hey.com>
2020-11-27 15:00:02 +0100

    feat(chapter-2): add port scanner

--- ch-2/dial/main.go
+++ ch-2/dial/main.go
@@ -3,11 +3,49 @@ package main
 import (
 	"fmt"
 	"net"
+	"sort"
 )
 
+func worker(ports chan int, results chan int) {
+	for p := range ports {
+		address := fmt.Sprintf("127.0.0.1:%d", p)
+		conn, err := net.Dial("tcp", address)
+		if err != nil {
+			results <- 0
+			continue
+		}
+		conn.Close()
+		results <- p
+	}
+}
+
 func main() {
-	_, err := net.Dial("tcp", "scanme.nmap.org:80")
-	if err == nil {
-		fmt.Println("Connection successful")
+	ports := make(chan int, 100)
+	results := make(chan int)
+	var openports []int
+
+	for i := 0; i < cap(ports); i++ {
+		go worker(ports, results)
+	}
+
+	go func() {
+		for i := 1; i <= 65535; i++ {
+			ports <- i
+		}
+	}()
+
+	for i := 0; i < 65535; i++ {
+		port := <-results
+		if port != 0 {
+			openports = append(openports, port)
+		}
+	}
+
+	close(ports)
+	close(results)
+
+	sort.Ints(openports)
+	for _, port := range openports {
+		fmt.Printf("%d open\n", port)
 	}
 }
